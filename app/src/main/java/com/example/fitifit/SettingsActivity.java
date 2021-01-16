b8f3ed0b3fd9f9719e93d5f1682b5b90143ff9d6
guneycansanli <48263266+guneycansanli@users.noreply.github.com>
2021-01-16 15:46:12 +0300

    Update 24.12.2020 -Added Settings and toke value from FireBase.... Update 24.12.2020 -Tried to crop from selected from phone ex. cammera or gallery for chance user s photo but version is for old version....need to try for new version Crop... Update 3.01.2021 -Added Register Selection Like DieticianRegisterActivity we can sign up like normal user or dietician..... Update 07.01.2021 -Started for Dietetic register...... Update 08.01.2021 -Started for admin login checking the DB for Admin.... -Update 08.01.2021 -Added Admin Login Panel And Reached Admin s main -Update 09.01.2021 -Created admin panel we can acess with admin profile to admin panel and we can list users from Db(FireBase) but we have a problem that is Toolbar Problem for AndroidX casting for Toolbar... It is diffrent for now we can use ignore Toolbar..... -Update 10.01.2021 -Admin s panel and users listing is working... Update 10.01.2021 -Added dieticians register panel... Update 16.01.20021 -Added Menu Item and started to Working on FireStore

--- app/src/main/java/com/example/fitifit/SettingsActivity.java
+++ app/src/main/java/com/example/fitifit/SettingsActivity.java
@@ -7,23 +7,33 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -35,7 +45,10 @@ public class SettingsActivity extends AppCompatActivity {
     private CircleImageView settingsImg;
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    private Boolean isCheck=false;
     private Uri imageUri=null;
+    private ProgressDialog settingsProgress;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +62,11 @@ public class SettingsActivity extends AppCompatActivity {
         settingsBtn=(Button) findViewById(R.id.settings_btn);
         settingsName=(EditText) findViewById(R.id.settings_name);
         settingsImg=(CircleImageView) findViewById(R.id.settings_image);
+        settingsProgress = new ProgressDialog(this);
+
+
         mAuth=FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
         String user_id=mAuth.getCurrentUser().getUid();
         mDatabase=FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
         mDatabase.addValueEventListener(new ValueEventListener() {
@@ -77,10 +94,29 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 ActivityCompat.requestPermissions(SettingsActivity.this,new String []{
                     Manifest.permission.READ_EXTERNAL_STORAGE},1);
-                CropImage.activity()
+                CropImage.activity(null)
                         .setGuidelines(CropImageView.Guidelines.ON)
                         .start(SettingsActivity.this);
 
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName=settingsName.getText().toString();
+
+                    settingsProgress.setTitle("Güncelleniyor");
+                    settingsProgress.setMessage("Bilgilerinizi Güncelliyoruz Lütfen Bekleyin");
+                    settingsProgress.show();
+                    if(!TextUtils.isEmpty(userName) && imageUri != null)
+                    {
+                        if(isCheck)
+                        {
+                            StorageReference userImage=storageReference.child("profile_images").child(user_id+".jpg");
+
+                        }
+                    }
             }
         });
 
@@ -90,14 +126,19 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE ) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 imageUri = result.getUri();
                 settingsImg.setImageURI(imageUri);
+                isCheck=true;
+                Toast.makeText(
+                        this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
+                Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
             }
         }
     }
+
+
 }
