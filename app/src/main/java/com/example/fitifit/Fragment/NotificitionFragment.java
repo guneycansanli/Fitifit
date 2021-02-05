f2c9891d3682a71b140f0ce987a11018fd71a1f9
guneycansanli <48263266+guneycansanli@users.noreply.github.com>
2021-02-05 11:47:37 +0300

    Update 24.12.2020 -Added Settings and toke value from FireBase.... Update 24.12.2020 -Tried to crop from selected from phone ex. cammera or gallery for chance user s photo but version is for old version....need to try for new version Crop... Update 3.01.2021 -Added Register Selection Like DieticianRegisterActivity we can sign up like normal user or dietician..... Update 07.01.2021 -Started for Dietetic register...... Update 08.01.2021 -Started for admin login checking the DB for Admin.... -Update 08.01.2021 -Added Admin Login Panel And Reached Admin s main -Update 09.01.2021 -Created admin panel we can acess with admin profile to admin panel and we can list users from Db(FireBase) but we have a problem that is Toolbar Problem for AndroidX casting for Toolbar... It is diffrent for now we can use ignore Toolbar..... -Update 10.01.2021 -Admin s panel and users listing is working... Update 10.01.2021 -Added dieticians register panel... Update 16.01.20021 -Added Menu Item and started to Working on FireStore Update 17.01.2021 -Added Fragments and Created new Adapter and model for user follows Update 17.01.2020 -After this time users can follow Dieticians and people will see their post about health Update 17.01.2021 -Added Post Share Fragment Bur Cropper Doesn t access yo Gllery()old version Update 18.01.2021 -Cropper is working with API 28 now Users can share photo and their posts have dataStore in Firebase also they had identificition in DataBase........ Update 24.01.2021 -Fixed some Layout Designs for working on different versions Update 29.01.2021 -Prepared home fragment for all users users can look dieticians posts--Layout Xml is okey-- Update 01.02.2021 -Fixed some Layout design and colors---added app icon Round style--- Update 02.02.2021 -Added Buttons for Chating with Dietician to activity_main.xml----- Update 04.02.2020 -Started for chat process--- Update 05.02.2021 -Created Chat panel, Used split fragment style and showed users..----------

--- app/src/main/java/com/example/fitifit/Fragment/NotificitionFragment.java
+++ app/src/main/java/com/example/fitifit/Fragment/NotificitionFragment.java
@@ -15,12 +15,12 @@ import com.example.fitifit.R;
  */
 public class NotificitionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -32,7 +32,7 @@ public class NotificitionFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment NotificitionFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static NotificitionFragment newInstance(String param1, String param2) {
         NotificitionFragment fragment = new NotificitionFragment();
         Bundle args = new Bundle();
--- app/src/main/java/com/example/fitifit/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/fitifit/Fragment/ProfileFragment.java
@@ -27,12 +27,12 @@ public class ProfileFragment extends Fragment {
 
     private Button btn_chat,btn_chance;
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -48,7 +48,7 @@ public class ProfileFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
+
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
