b559ce4ea819e195163b62cfe164487e78649a88
guneycansanli <48263266+guneycansanli@users.noreply.github.com>
2021-02-06 20:28:59 +0300

    Commit History--All Commits EveryTime Update 24.12.2020 -Added Settings and toke value from FireBase.... Update 24.12.2020 -Tried to crop from selected from phone ex. cammera or gallery for chance user s photo but version is for old version....need to try for new version Crop... Update 3.01.2021 -Added Register Selection Like DieticianRegisterActivity we can sign up like normal user or dietician..... Update 07.01.2021 -Started for Dietetic register...... Update 08.01.2021 -Started for admin login checking the DB for Admin.... -Update 08.01.2021 -Added Admin Login Panel And Reached Admin s main -Update 09.01.2021 -Created admin panel we can acess with admin profile to admin panel and we can list users from Db(FireBase) but we have a problem that is Toolbar Problem for AndroidX casting for Toolbar... It is diffrent for now we can use ignore Toolbar..... -Update 10.01.2021 -Admin s panel and users listing is working... Update 10.01.2021 -Added dieticians register panel... Update 16.01.20021 -Added Menu Item and started to Working on FireStore Update 17.01.2021 -Added Fragments and Created new Adapter and model for user follows Update 17.01.2020 -After this time users can follow Dieticians and people will see their post about health Update 17.01.2021 -Added Post Share Fragment Bur Cropper Doesn t access yo Gllery()old version Update 18.01.2021 -Cropper is working with API 28 now Users can share photo and their posts have dataStore in Firebase also they had identificition in DataBase........ Update 24.01.2021 -Fixed some Layout Designs for working on different versions Update 29.01.2021 -Prepared home fragment for all users users can look dieticians posts--Layout Xml is okey-- Update 01.02.2021 -Fixed some Layout design and colors---added app icon Round style--- Update 02.02.2021 -Added Buttons for Chating with Dietician to activity_main.xml----- Update 04.02.2020 -Started for chat process--- Update 05.02.2021 -Created Chat panel, Used split fragment style and showed users..---------- Update 06.02.2021 -Worked on chat panel Users with Dietician normally users can chatting with users but,it set chat with dietician also listin for dietician with using UserChatAdapter.java, MessageActivity, left and right view for appbar,,,lastly readMessages and sendMessage functions used with ChatMainActivity and UserAdapterfor listing message from last session........ Update 06.02.2021 -Added Listing Chat History and also Clickable and then access to chat panel for users to Dieticians........

--- app/src/main/java/com/example/fitifit/Fragment/ChatsFragment.java
+++ app/src/main/java/com/example/fitifit/Fragment/ChatsFragment.java
@@ -2,23 +2,124 @@ package com.example.fitifit.Fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.fitifit.Adapter.UserAdapter;
+import com.example.fitifit.Adapter.UserChatAdapter;
+import com.example.fitifit.Model.Chat;
+import com.example.fitifit.Model.UserProfileModel;
 import com.example.fitifit.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChatsFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
+    private UserChatAdapter userAdapter;
+    private List<UserProfileModel> mUsers;
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+
+    private List<String> usersList;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_chats,container,false);
+
+        recyclerView = view.findViewById(R.id.recycler_view_chats);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        usersList = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    usersList.clear();
+                    for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                        Chat chat=dataSnapshot.getValue(Chat.class);
+                        if(chat.getSender().equals(fuser.getUid())){
+                            usersList.add(chat.getReceiver());
+                        }
+                        if (chat.getReceiver().equals(fuser.getUid())){
+                            usersList.add(chat.getSender());
+                        }
+                    }
+                    readChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        return view;
+
     }
+
+    private void readChats(){
+        mUsers = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference("Dieticians");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUsers.clear();
+
+                // Display 1 user from chats
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    UserProfileModel user = dataSnapshot.getValue(UserProfileModel.class);
+
+                    for(String id : usersList){
+                        if (user.getUid().equals(id)){
+                            if (mUsers.size() != 0){
+                                for (UserProfileModel userl : mUsers){
+                                    if (!user.getUid().equals(userl.getUid())){
+                                        mUsers.add(user);
+                                    }
+                                }
+                            } else {
+                                mUsers.add(user);
+                            }
+                        }
+                    }
+                }
+                userAdapter = new UserChatAdapter(getContext(),mUsers);
+                recyclerView.setAdapter(userAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
